60c6e434cef8a0581e5c670f8afbef09f7e58b36
devpatelwebdeveloper <hello@devashishpatel.com>
2019-08-08 16:21:37 -0400

    Updates

---
+++ precache-manifest.4622b304a1ff55e3abc8ac2ba259c7b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf4c7ebe0cd4a45066f996ea7cec7aa",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "019f13314826185d3286",
+    "url": "/Monster-Rolodex/static/css/main.27cdb5ba.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "019f13314826185d3286",
+    "url": "/Monster-Rolodex/static/js/main.2a67c39d.chunk.js"
+  },
+  {
+    "revision": "1c53a097523ea440329e",
+    "url": "/Monster-Rolodex/static/js/runtime~main.5e73536d.js"
+  }
+]);
